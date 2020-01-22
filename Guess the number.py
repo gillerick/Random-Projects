a562cc3a18b14e44d35c3ceefee740db530354ed
Gill Erick <=>
2020-01-22 23:07:17 +0300

    Final code

--- Guess the number.py
+++ Guess the number.py
@@ -7,15 +7,22 @@ class Random:
         self.num = random.randrange(10) # Or randint(0, 10)
 
     def go(self):
-        if self.guess == self.num:
-            print(f"You guessed right. The correct number is {self.num}")
-        elif self.guess > self.num:
-            print(f"Ooops! Please try again. The number was too high")
-        elif self.guess < self.num:
-            print(f"Ooops! Please try again. The number was too low")
+        try:
+            if self.guess == self.num:
+                print(f"You guessed right. The correct number is {self.num}.")
+            elif self.guess > 10:
+                self.guess = int(input("The number entered is outside the range. Please try again!\n"))
+                Random.go(self)
+            elif self.guess > self.num:
+                self.guess = int(input(f"Ooops! Please try again. The number was too high.\n"))
+                Random.go(self)
+            elif self.guess < self.num:
+                self.guess = int(input(f"Ooops! Please try again. The number was too low.\n"))
+                Random.go(self)
+        except ValueError:
+            self.guess = int(input("Please enter an integer value.\n"))
 
 
 if __name__ == '__main__':
-    # Create a class instance called rand
     rand = Random()
     rand.go()
